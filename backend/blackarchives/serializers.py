223d87f916c2c0d0c40b5b85ecf77e97b2190ccd
Kiaya Anderson <kiayaanderson@Kiayas-MacBook-Air.local>
2022-01-27 10:20:17 -0500

    updated serializers

--- backend/blackarchives/serializers.py
+++ backend/blackarchives/serializers.py
@@ -5,7 +5,7 @@ from blackarchives.models import Director, Film
 class FilmSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Film
-        fields = ['title', 'year', 'description', 'image', 'director']
+        fields = ['id', 'title', 'year', 'description', 'image', 'director']
 
 
 class DirectorSerializer(serializers.HyperlinkedModelSerializer):
@@ -13,4 +13,4 @@ class DirectorSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = Director
-        fields = ['name', 'roles', 'biography', 'image', 'films']
+        fields = ['id', 'name', 'roles', 'biography', 'image', 'films']
