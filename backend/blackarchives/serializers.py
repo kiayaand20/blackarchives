5de92f05463c7790c9811e666800b6fca5997e11
Kiaya Anderson <kiayaanderson@Kiayas-Air.lan>
2022-01-28 14:04:23 -0500

    updated serializers and models

--- backend/blackarchives/serializers.py
+++ backend/blackarchives/serializers.py
@@ -7,7 +7,7 @@ class FilmSerializer(serializers.HyperlinkedModelSerializer):
   
     class Meta:
         model = Film
-        fields = ['id', 'title', 'release', 'description', 'image', 'director']
+        fields = ['id', 'title', 'description', 'image', 'director']
 
 class DirectorSerializer(serializers.HyperlinkedModelSerializer):
     films = FilmSerializer(many=True)
